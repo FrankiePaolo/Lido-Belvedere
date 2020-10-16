0c1a47864fabddbf307193a72d4edaf8fb55cb0a
Frank <francescopaolo.castiglione@community.unipa.it>
2020-10-16 17:13:36 +0200

    Minor

--- Lido/WebContent/js/book_spot.js
+++ Lido/WebContent/js/book_spot.js
@@ -5,5 +5,9 @@
 $(document).ready(function(){
 	var current_date=new Date();
 	var time=(date.getHours() <=12? "Afternoon" : "Morning");
-
+	
+	var $inputDate=$("input[name='date']");
+	
+	//We get only the date
+	$inputDate.attr("min",date.toIsoString().split("T")[0]);
 });
