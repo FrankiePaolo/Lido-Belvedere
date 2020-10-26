fcbf4fca2619f7bd32cf1bf1bb1bbda71eb006f3
Frank <francescopaolo.castiglione@community.unipa.it>
2020-10-26 19:49:49 +0100

    Map

--- Lido/WebContent/js/beach_spots.js
+++ Lido/WebContent/js/beach_spots.js
@@ -21,20 +21,20 @@ function loadMap (inputDate,inputTime) {
         success: function(json) {
 			$('.map').html("<div id=\"mapRow\"class=\"row\">");
 			for(var i=1;i<=json.length;i++){
-				$('#mapRow').append("<div id= "+ i + " class=\"col\"><img src=\"/Lido/img/sunbed.png\" alt=\"Spot "+i+" \" class=\"img-thumbnail\"></div>");
+				$('#mapRow').append("<div id= \""+ i + "\" class=\"col\"><div>");
 				if(i % 3 == 0){
 					$('#mapRow').append("<div class=\"w-100\"></div>");
 				}
 			}
 			$('.map').append("<\div>");
-            //json.forEach(insertSlot);
+            json.forEach(insertSlot);
         },
         error: $('.map').html('<h2>Error loading map from server, please try again later.</h2>')
     });
 }
 
-/*
+//		<div id= "+ i + " class=\"col\"><img src=\"/Lido/img/sunbed.png\" alt=\"Spot "+i+" \" class=\"img-thumbnail\"></div>
+
 function insertSlot(slot) {
-	
+	$("#"+slot.id).replaceWith("<div class=\"col\"><img src=\"/Lido/img/sunbed.png\" class=\"img-thumbnail\"></div>");
 }
-*/
