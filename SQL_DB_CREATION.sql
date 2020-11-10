8e23f1d6425388c46d599edfdb8247230e9f4abd
Frank <francescopaolo.castiglione@community.unipa.it>
2020-11-10 11:20:42 +0100

    SQL_DB_CREATION

---
+++ SQL_DB_CREATION.sql
@@ -0,0 +1,30 @@
+SELECT * FROM Booking;
+
+SELECT * FROM User;
+
+SELECT * FROM Food_Item;
+
+SELECT * FROM Booking where User_ID='7';
+
+create table `Order`
+(
+    ID     int auto_increment
+        primary key,
+    User_ID int                                                              not null,
+    State       enum ('waiting', 'ready', 'delivered') default 'waiting'         null,
+    Date        datetime                               default CURRENT_TIMESTAMP null,
+    constraint fk_Order_User
+        foreign key (User_ID) references User (ID)
+);
+
+create table Order_has_Food_Item
+(
+    Order_ID         int not null,
+    Food_Item_ID 	 int not null,
+    Amount                int null,
+    primary key (Order_ID, Food_Item_ID),
+    constraint fk_Order_has_Food_Item
+        foreign key (Food_Item_ID) references Food_Item (ID),
+    constraint fk_Order_has_Food_Item2
+        foreign key (Order_ID) references `Order` (ID)
+);
