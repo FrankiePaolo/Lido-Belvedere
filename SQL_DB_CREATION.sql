7885ceea893015ce72e0ee92116c17167387abf0
Frank <francescopaolo.castiglione@community.unipa.it>
2020-11-25 15:29:54 +0100

    SQL

--- SQL_DB_CREATION.sql
+++ SQL_DB_CREATION.sql
@@ -1,3 +1,50 @@
+create database if not exists lido_test collate latin1_swedish_ci;
+
+use lido_test;
+
+create table Chair
+(
+    ID int auto_increment
+        primary key
+);
+
+create table Food_Item
+(
+    ID int auto_increment
+        primary key,
+    Name        varchar(45)  not null,
+    Price       float        not null,
+    Description varchar(128) not null,
+    Category    varchar(45)  not null
+);
+
+create table User
+(
+    ID   int auto_increment
+        primary key,
+    FirstName     varchar(45)                    not null,
+    LastName  varchar(45)                    not null,
+    Email     varchar(45)                    null,
+    Password varchar(64)                    not null,
+    Role     varchar(45) default 'Customer' not null,
+    unique (Email)
+);
+
+create table Booking
+(
+    Date              date                          not null,
+    Time              enum ('AM', 'PM', 'Full day') not null,
+    Chair_ID int                           not null,
+    ID         int auto_increment
+        primary key,
+    User_ID       int                           not null,
+    constraint fk_Booking_Chair
+        foreign key (Chair_ID) references Chair (ID),
+    constraint fk_Booking_User
+        foreign key (User_ID) references User (ID)
+);
+
+
 create table `Order`
 (
     ID     int auto_increment
