a61834126fecd34a5f802d8d07be1ce1bf3a3b18
Frank <francescopaolo.castiglione@community.unipa.it>
2020-11-25 15:33:23 +0100

    Cleaner

--- SQL_DB_CREATION.sql
+++ SQL_DB_CREATION.sql
@@ -22,9 +22,9 @@ create table User
 (
     ID   int auto_increment
         primary key,
-    FirstName     varchar(45)                    not null,
-    LastName  varchar(45)                    not null,
-    Email     varchar(45)                    null,
+    FirstName     varchar(45)               not null,
+    LastName  varchar(45)                   not null,
+    Email     varchar(45)                   null,
     Password varchar(64)                    not null,
     Role     varchar(45) default 'Customer' not null,
     unique (Email)
@@ -34,10 +34,10 @@ create table Booking
 (
     Date              date                          not null,
     Time              enum ('AM', 'PM', 'Full day') not null,
-    Chair_ID int                           not null,
+    Chair_ID int                          	      not null,
     ID         int auto_increment
         primary key,
-    User_ID       int                           not null,
+    User_ID       int                               not null,
     constraint fk_Booking_Chair
         foreign key (Chair_ID) references Chair (ID),
     constraint fk_Booking_User
@@ -58,9 +58,9 @@ create table `Order`
 
 create table Order_has_Food_Item
 (
-    Order_ID         int not null,
+    Order_ID         	 int not null,
     Food_Item_ID 	 int not null,
-    Amount                int null,
+    Amount              int null,
     primary key (Order_ID, Food_Item_ID),
     constraint fk_Order_has_Food_Item
         foreign key (Food_Item_ID) references Food_Item (ID),
